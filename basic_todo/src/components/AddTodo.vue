77dfdf9b91c90415f49284ae7f3343df7b255913
Shiri <54873733+shiri87@users.noreply.github.com>
2020-08-06 21:48:05 +1000

    debugging + refactorying

---
+++ basic_todo/src/components/AddTodo.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    type="text"
+    class="w-100 p-2"
+    placeholder="Type Todol list"
+    v-model="todoText"
+    @keyup.enter="addTodo"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return { todoText: "" }
+  },
+  methods: {
+    addTodo(e) {
+      this.$emit("add-todo", e.target.value)
+    },
+  },
+}
+</script>
+
+<style></style>
